ead0126c8eded0e3163613173b8d7ef414858b69
jphoubion <jp.houbion@gmail.com>
2023-01-05 22:31:45 +0100

    add button fuction created loading from json file -> combobox getting address information when selecting value in combobox

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import sys
+import json
+from functools import partial
+
 # from pprint import pprint
 # from openpyxl import Workbook
 # from openpyxl.styles import PatternFill, Font, Color
@@ -7,8 +10,10 @@ import sys
 
 from PySide6 import QtWidgets, QtCore
 from PySide6.QtCore import Qt
+from PySide6.QtGui import QPixmap, QIcon, QIconEngine
 
-from PySide6.QtWidgets import QDialog, QFileDialog, QLabel, QMessageBox
+from PySide6.QtWidgets import QDialog, QFileDialog, QLabel, QMessageBox, QComboBox, QPushButton, QTableWidgetItem, \
+    QTextEdit
 
 from ui.mainwindow import Ui_MainWindow
 import comanagementwindow
@@ -23,7 +28,36 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
 
         self.COManagementWindow = comanagementwindow.COMainWindow()
 
+        # Chargement du fichier JSON des C/O
+        self.data = self.load_data()
 
+        # Ajout de la 1ère ligne de la table
+        self.btn_pressed()
+        self.btnAddRow.clicked.connect(self.btn_pressed)
+
+    def btn_pressed(self):
+        self.tw_co.insertRow(self.tw_co.rowCount())
+
+        te_coordonnees = QTextEdit()
+        self.tw_co.setCellWidget(self.tw_co.rowCount()-1, 1, te_coordonnees)
+
+        combo_co = QComboBox()
+        combo_co.addItems(self.data['co'])
+        # combo_co.activated.connect(partial(self.get_co_details, combo_co.currentText()))
+        combo_co.activated.connect(lambda x: te_coordonnees.setText(self.data['co'][combo_co.currentText()]['adresse']))
+        self.tw_co.setCellWidget(self.tw_co.rowCount()-1, 0, combo_co)
+
+    def load_data(self):
+        with open("data.json", "r") as f:
+            data = json.load(f)
+        # print(data['co'])
+        for d in data['co']:
+            print(data['co'][d])
+            # print(d['co']['adresse'])
+        return data
+
+    def get_co_details(self, co_name, idx):
+        return self.data['co'][co_name]['adresse']
     def openCOManagementWindow(self):
         self.COManagementWindow.show()
 
