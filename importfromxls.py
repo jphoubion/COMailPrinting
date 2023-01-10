497eddc2cec20983322dc8b83a3a91875382dcea
Houbion <Jp.houbion@gmail.com>
2023-01-10 16:48:38 +0100

    complete change

---
+++ importfromxls.py
@@ -0,0 +1,65 @@
+import json
+
+from PySide6.QtWidgets import QFileDialog
+from openpyxl import Workbook, load_workbook
+
+class ImportFromXls:
+
+    def __init__(self):
+
+        self.wb = Workbook()
+        self.sheet = None
+
+    def select_customers_file(self):
+        dlg = QFileDialog(filter="*.xlsx;;*.xls")
+        dlg.setWindowTitle("Selectionner le fichier des clients")
+        dlg.setFileMode(QFileDialog.ExistingFile)
+        dlg.setViewMode(QFileDialog.Detail)
+        if dlg.exec():
+            self.filename = dlg.selectedFiles()
+            self.wb = load_workbook(self.filename[0])
+            return self.wb.active
+
+    def import_customers(self):
+        self.sheet = self.select_customers_file()
+        customer_dict = {}
+        if self.sheet is not None:
+            with open("customers.json", "w") as outfile:
+                for row in self.sheet.iter_rows(min_row=2):
+                    # building DIC to export to JSON
+                    # cleaning C/O information
+                    co = str(row[3].value).split(' ')[-2:]
+                    co_name = f"{co[-2]} {co[-1]}".upper().replace("C/O ME ",'').replace("C/O ME. ",'')
+                    customer_dict.update({
+                        row[0].value: {
+                            "name": row[2].value,
+                            "co": co_name
+                        }
+                    })
+                # print(customer_dict)
+                json_object = json.dumps(customer_dict, indent=4)
+
+                # Writing to sample.json
+                outfile.write(json_object)
+
+                self.import_co()
+
+    def import_co(self):
+        if self.sheet is not None:
+            co_dict = {}
+            with open("co.json", "w") as outfile:
+                for row in self.sheet.iter_rows(min_row=2):
+                    co_name = str(row[3].value).upper().replace("C/O ME ",'').replace("C/O ME. ",'')
+                    co_dict.update({
+                        co_name: {
+                            "co_name": co_name,
+                            "adress" : row[6].value,
+                            "cp" : row[8].value,
+                            "city" : row[9].value
+                        }
+                    })
+                # print(customer_dict)
+                json_object = json.dumps(co_dict, indent=4)
+
+                # Writing to sample.json
+                outfile.write(json_object)
--- main.py
+++ main.py
@@ -3,19 +3,14 @@ sys.argv += ['-platform', 'windows:darkmode=2']
 import json
 from functools import partial
 
-# from pprint import pprint
-# from openpyxl import Workbook
-# from openpyxl.styles import PatternFill, Font, Color
-# from openpyxl.utils import get_column_letter
-
-
 from PySide6 import QtWidgets, QtCore
 from PySide6.QtCore import Qt
 from PySide6.QtGui import QPixmap, QIcon, QIconEngine
 
 from PySide6.QtWidgets import QDialog, QFileDialog, QLabel, QMessageBox, QComboBox, QPushButton, QTableWidgetItem, \
-    QTextEdit
+    QTextEdit, QCompleter
 
+from  importfromxls import ImportFromXls
 
 from ui.mainwindow import Ui_MainWindow
 import comanagementwindow
@@ -25,66 +20,110 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         super(MainWindow, self).__init__()
         self.setupUi(self)
 
+        self.import_from_xls = ImportFromXls()
         self.actionQuitter.triggered.connect(self.quit)
         self.actionGestion.triggered.connect(self.openCOManagementWindow)
+        self.actionImport_customers.triggered.connect(self.import_from_xls.import_customers)
 
         self.COManagementWindow = comanagementwindow.COMainWindow()
 
+        self.company_data = self.load_company_data()
+
         # Chargement du fichier JSON des C/O
-        self.data = self.load_co_data()
+        self.co_data = self.load_co_data()
 
         # Loading customer from JSON
         self.customers_data = self.load_customers_data()
 
-
         # ComboBox des sociétés
-        self.cbb_company.addItems(self.data['companies'])
+        self.cbb_company.addItems(self.company_data['companies'])
 
         # Ajout de la 1ère ligne de la table
         self.btn_pressed()
         self.btnAddRow.clicked.connect(self.btn_pressed)
+        #
+        # self.tw_co.setColumnWidth(3, 290)
+        # print(self.tw_co.columnWidth(3))
 
     def btn_pressed(self):
         # Adding a row in the table
         self.tw_co.insertRow(self.tw_co.rowCount())
 
-        # Add a QTextEdit on the second column
-        te_coordonnees = QTextEdit()
-        self.tw_co.setCellWidget(self.tw_co.rowCount()-1, 1, te_coordonnees)
+        ###################################################################################
+        # Add combobox for CUSTOMERS on each new line and fill it with customers from JSON
+        ###################################################################################
+        combo_cust = QComboBox()
+        combo_cust.setEditable(True)
+        combo_cust.setFixedWidth(100)
 
-        # Add a combobox on the first column and fill it with all C/O from JSON
+        customer_list = []
+        for cust in self.customers_data:
+            customer_list.append(cust)
+        combo_cust.addItems(customer_list)
+
+        combo_cust.currentIndexChanged.connect(self.select_co_from_customer)
+
+        self.tw_co.setCellWidget(self.tw_co.rowCount() - 1, 0, combo_cust)
+
+
+        ###################################################################################
+        # Add combobox for C/O on each new line and fill it with customers from JSON
+        ###################################################################################
         combo_co = QComboBox()
-        combo_co.addItems(self.data['co'])
-        combo_co.currentTextChanged.connect(partial(self.get_co_details, te_coordonnees, combo_co.currentText()))
-        # combo_co.activated.connect(lambda x: te_coordonnees.setText(self.data['co'][combo_co.currentText()]['adresse']))
-        self.tw_co.setCellWidget(self.tw_co.rowCount()-1, 0, combo_co)
+        combo_co.setEditable(True)
+        combo_co.setFixedWidth(150)
+
+        co_list = []
+        for co_name in self.co_data:
+            co_list.append(self.co_data[co_name]['co_name'])
+        combo_co.addItems(co_list)
+        # combo_co.currentTextChanged.connect(partial(self.get_co_details, te_coordonnees, combo_co.currentText()))
+
+        self.tw_co.setCellWidget(self.tw_co.rowCount()-1, 1, combo_co)
+
+        ###################################################################################
+        # Add field to display C/O info
+        ###################################################################################
+        te_coordonnees = QTextEdit()
+        te_coordonnees.setFixedWidth(200)
+        self.tw_co.setCellWidget(self.tw_co.rowCount()-1, 2, te_coordonnees)
+
+        ###################################################################################
+        # Add button to update data about C/O in JSON file
+        ###################################################################################
+        btn_update_co = QPushButton("MAJ")
+        self.tw_co.setCellWidget(self.tw_co.rowCount()-1, 5, btn_update_co)
+        # Add a combobox on the 1st column and fill it with all C/O from JSON
 
         # Fill the QTextEdit in with current selected CO address
-        self.get_co_details(te_coordonnees, combo_co.currentText(), combo_co.currentText())
+        # self.get_co_details(te_coordonnees, combo_co.currentText(), combo_co.currentText())
 
-        # Add combobox for customers on each new line and fill it with customers from JSON
-        combo_cust = QComboBox()
-        combo_cust.addItems(self.customers_data['customers'])
-        self.tw_co.setCellWidget(self.tw_co.rowCount() - 1, 2, combo_cust)
-
+        ###################################################################################
         # Add combobox for company on each new line and fill it with companies from JSON and
         # display the current default company
+        ###################################################################################
         combo_company = QComboBox()
-        combo_company.addItems(self.data["companies"])
+        combo_company.addItems(self.company_data["companies"])
         combo_company.setCurrentText(self.cbb_company.currentText())
-        print(combo_company.itemText(0))
+        # print(combo_company.itemText(0))
         self.tw_co.setCellWidget(self.tw_co.rowCount()-1, 4, combo_company)
 
         self.tw_co.resizeColumnsToContents()
+        # self.tw_co.setColumnWidth(3, 290)
+
+    def load_company_data(self):
+        with open("companies.json", "r") as f:
+            data = json.load(f)
+        return data
 
     def load_co_data(self):
-        with open("data.json", "r") as f:
+        with open("co.json", "r") as f:
             data = json.load(f)
         # print(data['co'])
         # print(data['companies'])
         # for d in data['co']:
         #     print(data['co'][d])
-            # print(d['co']['address'])
+        # print(d['co']['address'])
         return data
 
     def load_customers_data(self):
@@ -93,7 +132,7 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         return data
 
     def select_co_from_customer(self):
-        pass
+        print('select co from customer')
 
     def get_co_details(self, te_box, ol_co_name, new_co_name):
         # print(te_box, new_co_name)
